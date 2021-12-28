9265348e1da11de085b3b07626d36ff5e841a3c1
Shreyash Mishra <shreyashm1601@gmail.com>
2021-12-28 13:06:48 +0530

    userpage basic add

--- event-booking/src/Pages/UserPage.jsx
+++ event-booking/src/Pages/UserPage.jsx
@@ -9,6 +9,10 @@ import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
 import Typography from '@material-ui/core/Typography';
 import '../Components/Styling/LoginPag.css'
+import '../Components/Styling/UserPage.css'
+import { PopularEvents } from "../Components/HomePage/PopularEvents";
+import { PremierMovies } from "../Components/HomePage/PremierMovies";
+import { RecommendedMovies } from "../Components/HomePage/RecommendedMovies";
 
 const styles = (theme) => ({
   root: {
@@ -51,7 +55,7 @@ const DialogActions = withStyles((theme) => ({
 }))(MuiDialogActions);
 
 export default function UserPage({ action, handleCloseLogin }) {
-
+  
 
 
 
@@ -89,9 +93,34 @@ export default function UserPage({ action, handleCloseLogin }) {
           <Button color="primary">
             Sign in
           </Button>
-        
         </DialogActions>
       </Dialog>
+      <div
+        className="container_user"
+        style={{
+          backgroundImage: "linear-gradient(315deg, #f9484a 0%, #fbd72b 74%)",
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+        }}
+      >
+
+      </div>
+      <img className="profilepic" src="https://imgresizer.eurosport.com/unsafe/1200x0/filters:format(webp):focal(1390x540:1392x538)/origin-imgresizer.eurosport.com/2021/08/05/3195392-65463108-2560-1440.jpg"/>
+      <div className="userdetails">
+        <h1>Lionel Messi</h1>
+        <h4>Lionel Andrés Messi, also known as Leo Messi, is an Argentine professional footballer who plays as a forward for Ligue 1 club Paris Saint-Germain and captains the Argentina national team. Often considered the best player in the world and widely regarded as one of the greatest players of all time, Messi has won a record seven Ballon d'Or awards, a record six European Golden Shoes, and in 2020 was named to the Ballon d'Or Dream Team. Until leaving the club in 2021, he had spent his entire professional career with Barcelona, where he won a club-record 35 trophies, including ten La Liga titles, seven Copa del Rey titles and four UEFA Champions Leagues. A prolific goalscorer and creative playmaker, Messi holds the records for most goals in La Liga (474), a La Liga and European league season (50), most hat-tricks in La Liga (36) and the UEFA Champions League (8), and most assists in La Liga (192), a La Liga season (21) and the Copa América (17). He also holds the record for most international goals by a South American male (80). Messi has scored over 750 senior career goals for club and country, and has the most goals by a player for a single club.</h4>
+      </div>
+      <br/><br/><br/><br/>
+
+      <PremierMovies />
+        <div style={{ backgroundColor: "#16161D" }}>
+          <RecommendedMovies />
+        </div>
+        <div style={{ backgroundColor: "#16161D" }}>
+          <PopularEvents />
+        </div>
+      <br/><br/><br/>
     </div>
   );
 }
