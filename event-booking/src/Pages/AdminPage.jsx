0a7aaca9fc2445d4e5de0551120bec2a5d81dfef
Siddharth <siddharth25pandey@gmail.com>
2021-12-28 19:56:24 +0530

    Login@v1

--- event-booking/src/Pages/AdminPage.jsx
+++ event-booking/src/Pages/AdminPage.jsx
@@ -1,70 +1,81 @@
-import React from 'react';
-import { withStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import Dialog from '@material-ui/core/Dialog';
-import MuiDialogContent from '@material-ui/core/DialogContent';
-import MuiDialogActions from '@material-ui/core/DialogActions';
+import React, { useState } from 'react';
 import "../Components/Styling/admin.css";
-import jsondata from "../scraped_data/db.json"
+import jsondata from "../scraped_data/db.json";
 
 
 
 
-// const DialogContent = withStyles((theme) => ({
-//   root: {
-//     padding: theme.spacing(2),
-//   },
-// }))(MuiDialogContent);
 
-// const DialogActions = withStyles((theme) => ({
-//   root: {
-//     margin: 0,
-//     padding: theme.spacing(1),
-//   },
-// }))(MuiDialogActions);
 
-export default function AdminPage({ action, handleCloseLogin }) {
+export default function AdminPage() {
   var feedback_data = jsondata.feed;
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(e.target.email.value);
+
+    if (!e.target.email.value) {
+      alert("Email is required");
+  
+    } else if (!e.target.email.value) {
+      alert("Valid email is required");
+
+    } else if (!e.target.password.value) {
+      alert("Password is required");
+    
+    } else if (
+      e.target.email.value === "admin@gmail.com" &&
+      e.target.password.value === "admin"
+    ) {
+      alert("Successfully logged in");
+     
+      e.target.email.value = "";
+      e.target.password.value = "";
+      //return (<UserPage/>);
+      
+    } else {
+      alert("Wrong email or password combination");
+      
+    }
+  }
+
   return (
-    <div>
-      {/* <Dialog onClose={handleCloseLogin} aria-labelledby="customized-dialog-title" open={action}>
-        
-        <DialogContent dividers style={{textAlign:'center'}}>
-         
-          <Button>Admin</Button>
-          <Button>User</Button>
-          
-        </DialogContent>
-        <DialogActions>
-          <Button  color="primary">
-            Sign in
-          </Button>
-        
-        </DialogActions>
-      </Dialog> */}
+    <div className="Login">
+      
+        <center>
+          <form className="form" onSubmit={handleSubmit}>
+            <div className="input-group">
+              <label htmlFor="email">Email</label>
+              <input type="email" name="email" placeholder="Enter your email" />
+            </div>
+            <div className="input-group">
+              <label htmlFor="password">Password</label>
+              <input type="password" name="password" placeholder="Enter your password" />
+            </div>
+            <button className="primary">Login</button>
+          </form>
+          <br />
+          <table className="styled-table">
+            <thead>
+              <tr>
+                <th scope="col">S.No</th>
+                <th scope="col">Name</th>
+                <th scope="col">Feedback</th>
+              </tr>
+            </thead>
+            <tbody>
+              {feedback_data.map((i) => (
+                <tr key={i.id}>
+                  <td>{i.id}</td>
+                  <td>{i.name}</td>
+                  <td>{i.feed}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
 
-<center>
+        </center>
+      
 
-<table className="styled-table">
-  <thead>
-    <tr>
-    <th scope="col">S.No</th>
-      <th scope="col">Name</th>
-      <th scope="col">Feedback</th>
-    </tr>
-  </thead>
-  <tbody>
-    {feedback_data.map((i) => (
-            <tr key={i.id}>
-              <td>{i.id}</td>
-              <td>{i.name}</td>
-              <td>{i.feed}</td>
-            </tr>
-    ))}
-  </tbody>
-</table>
-</center>
     </div>
   );
 }
-
--- event-booking/src/Pages/LoginPage.jsx
+++ event-booking/src/Pages/LoginPage.jsx
@@ -44,4 +44,3 @@ export default function Login({ action, handleCloseLogin }) {
     </div >
   );
 }
-
