fa1f55204f15db89d848495d7027c4d12e198d95
Siddharth <siddharth25pandey@gmail.com>
2022-03-28 15:29:36 +0530

    Mongoo Conneted

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,10 @@
 const express = require("express");
+const cors = require('cors');
+const mongoose =require('mongoose');
+
+
+require('dotenv').config();
+
 const app = express();
 
 app.get("/", (req, res) => {
@@ -8,6 +14,16 @@ app.post("/post", (req, res) => {
     console.log("Connected to React");
     res.redirect("/");
   });
-const PORT = process.env.PORT || 4000 ;
+const port = process.env.port || 4000 ;
 
-app.listen(PORT, console.log(`Server started on port ${PORT}`));
+app.listen(port, console.log(`Server started on port ${port}`));
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri);
+
+const connection = mongoose.connection;
+connection.once('open',()=>{
+    console.log("MongoDb Connection is Successful");
+});
+
+app.use(express.json())
