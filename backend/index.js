60babf22c0209274ef6f3878c1163f3e1e0bdfa7
Siddharth <siddharth25pandey@gmail.com>
2022-03-30 17:00:11 +0530

    tp middleware

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,31 @@
 const express = require("express");
+const path = require('path');
+const http = require('http');
+const port = process.env.port || 4000 ;
 const cors = require('cors');
 const mongoose =require('mongoose');
+const cookieParser = require('cookie-parser');
+const bparser = require('body-parser')
+var csrf = require('csurf')
+const fs = require('fs');
+const fsr = require('file-stream-rotator');
+
+const morgan = require('morgan')
+const multer = require('multer')
 
 
 require('dotenv').config();
 
 const app = express();
 app.use(cors());
+
+var parseForm = bparser.urlencoded({ extended: false });
+var csrfProtect = csrf({ cookie: true })
+
+app.use(bparser.urlencoded({ extended: false }))
+app.use(cookieParser())
+
+
 app.get("/", (req, res) => {
 res.send("Hello World!");
 });
@@ -14,7 +33,7 @@ app.post("/post", (req, res) => {
     console.log("Connected to React");
     res.redirect("/");
   });
-const port = process.env.port || 4000 ;
+
 
 app.listen(port, console.log(`Server started on port ${port}`));
 
