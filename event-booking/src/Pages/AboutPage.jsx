f846b8279b5dda16095411326ab6e5d995f76904
Priyam Bajpai <56591603+prichoms@users.noreply.github.com>
2022-04-27 01:17:32 +0530

    Update AboutPage.jsx

--- event-booking/src/Pages/AboutPage.jsx
+++ event-booking/src/Pages/AboutPage.jsx
@@ -82,7 +82,7 @@ export default function AboutPage({ action, handleCloseLogin }) {
             <div class="content">
                 <img src={naveen}
                     alt=""/>
-                <strong>Naveen Kumar</strong>
+                <strong>Naveen Vula</strong>
                 <p>S20190010192</p>
             </div>
             <div class="btn">
